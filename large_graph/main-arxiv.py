029d407c115b10f36b86511517adb42e1ed39583
Baskaran Sripathmanathan <baskaran.sripathmanathan@pmb.ox.ac.uk>
2025-04-17 17:44:40 +0000

    debug stuff as to why the output is different when nodes are reordered

--- large_graph/main-arxiv.py
+++ large_graph/main-arxiv.py
@@ -4,10 +4,12 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.utils import to_undirected, remove_self_loops, add_self_loops
+from torch_geometric.utils import to_undirected, remove_self_loops, add_self_loops, sort_edge_index
 
 from torch_geometric.data import Data
+from torch_geometric.loader import NeighborLoader
 import torchmetrics as tm
+from torch_geometric.nn.models import GCN
 
 
 from lg_parse import parse_method, parser_add_main_args
@@ -65,10 +67,11 @@ dataset.graph['edge_index'], _ = add_self_loops(dataset.graph['edge_index'], num
 dataset.graph['edge_index'], dataset.graph['node_feat'] = dataset.graph['edge_index'].to(device), dataset.graph['node_feat'].to(device)
 data = Data(x = dataset.graph['node_feat'], y = dataset.label.squeeze(1), edge_index = dataset.graph['edge_index'])
 data = data.to(device)
-print(f"DEBUG: x: {data.x.device}, y:{data.y.device}")
 
 ### Load method ###
 model = parse_method(args, n, c, d, device)
+#model = GCN(-1, args.hidden_channels, args.local_layers, c).to(device)
+
 
 criterion = nn.CrossEntropyLoss()
 eval_func = eval_acc
@@ -89,21 +92,45 @@ for run in range(args.runs):
     if args.save_model:
         save_model(args, model, optimizer, run)
 
+    train_loader = NeighborLoader(
+            data,
+            input_nodes = split_idx['train'],
+            num_neighbors = [data.num_nodes] * 100,
+            batch_size = data.num_nodes,
+            num_workers = 2,
+            pin_memory = True
+            )
+
+
     for epoch in range(args.epochs):
 
         model.train()
         optimizer.zero_grad()
 
-        out = model(data.x, data.edge_index)
-        loss = criterion(
-            out[train_idx], data.y[train_idx])
+        #The following are true:
+        # torch.all(sort_edge_index(batch.n_id.to(device)[batch.edge_index]) == sort_edge_index(data.edge_index))
+        # torch.all(data.x[batch.n_id] == batch.x)
+        # So we expect that
+        # out1[batch.n_id] == out
+
+        batch = next(iter(train_loader))
+        out1 = model(data.x, data.edge_index)
+        loss1 = criterion(
+            out1[train_idx], data.y[train_idx])
+        #out = model(data.x, data.edge_index) this has an error (with dropout set to 0) if 1e-4 if rerun
+        out = model(batch.x, batch.edge_index)
+        #breakpoint()
+        split_size = train_idx.shape[0]
+        loss = criterion(out[:split_size], batch.y[:split_size])
+        #breakpoint()
+        print(f"DEBUG: labels same:{(data.y[train_idx] == batch.y[:split_size]).all()}")
+        print(f"DEBUG: edge_indices same: {torch.all(sort_edge_index(batch.n_id.to(device)[batch.edge_index]) == sort_edge_index(data.edge_index))}")
+        print(f"DEBUG: data same:{ torch.all(data.x[batch.n_id] == batch.x) }")
+        print(f"DEBUG: out diff: {(out[batch.n_id]-out1).norm()}")
         loss.backward()
         optimizer.step()
 
-        #result = evaluate(model, dataset, split_idx, eval_func, criterion, args)
         result = evaluate_tm(model, dataset, split_idx, eval_obj, criterion, args)
-        #diffs = max([ torch.abs(acc1-acc2)  for acc1, acc2 in zip(result[:-1], result_tm[:-1]) ])
-        #print(f"BASKY: max diff in acc: {diffs}")
 
         logger.add_result(run, result[:-1])
 
