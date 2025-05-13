6037ac0b151112f053b2dcbe09b2beeb5eb63425
Baskaran Sripathmanathan <baskaran.sripathmanathan@pmb.ox.ac.uk>
2025-05-13 09:36:48 +0100

    add reddit and reformat

--- large_graph/dataset.py
+++ large_graph/dataset.py
@@ -6,7 +6,8 @@ from ogb.nodeproppred import NodePropPredDataset
 import numpy as np
 import scipy.sparse as sp
 from os import path
-#from google_drive_downloader import GoogleDriveDownloader as gdd
+
+# from google_drive_downloader import GoogleDriveDownloader as gdd
 import gdown
 import scipy
 
@@ -43,7 +44,13 @@ class NCDataset(object):
         self.graph = {}
         self.label = None
 
-    def get_idx_split(self, split_type='random', train_prop=.6, valid_prop=.2, label_num_per_class=20):
+    def get_idx_split(
+        self,
+        split_type="random",
+        train_prop=0.6,
+        valid_prop=0.2,
+        label_num_per_class=20,
+    ):
         """
         train_prop: The proportion of dataset for train split. Between 0 and 1.
         valid_prop: The proportion of dataset for validation split. Between 0 and 1.
@@ -52,40 +59,65 @@ class NCDataset(object):
         return split_idx
 
     def __getitem__(self, idx):
-        assert idx == 0, 'This dataset has only one graph'
+        assert idx == 0, "This dataset has only one graph"
         return self.graph, self.label
 
     def __len__(self):
         return 1
 
     def __repr__(self):
-        return '{}({})'.format(self.__class__.__name__, len(self))
+        return "{}({})".format(self.__class__.__name__, len(self))
 
 
-def load_dataset(data_dir, dataname, sub_dataname=''):
-    """ Loader for NCDataset
-        Returns NCDataset
+def load_dataset(data_dir, dataname, sub_dataname=""):
+    """Loader for NCDataset
+    Returns NCDataset
     """
     print(dataname)
-    if dataname in  ('amazon-photo', 'amazon-computer'):
+    if dataname in ("amazon-photo", "amazon-computer"):
         dataset = load_amazon_dataset(data_dir, dataname)
-    elif dataname in  ('coauthor-cs', 'coauthor-physics'):
+    elif dataname in ("coauthor-cs", "coauthor-physics"):
         dataset = load_coauthor_dataset(data_dir, dataname)
-    elif dataname in ('roman-empire', 'amazon-ratings', 'minesweeper', 'tolokers', 'questions'):
+    elif dataname in (
+        "roman-empire",
+        "amazon-ratings",
+        "minesweeper",
+        "tolokers",
+        "questions",
+    ):
         dataset = load_hetero_dataset(data_dir, dataname)
-    elif dataname == 'wikics':
+    elif dataname == "wikics":
         dataset = load_wikics_dataset(data_dir)
-    elif dataname in ('ogbn-arxiv', 'ogbn-products'):
+    elif dataname in ("ogbn-arxiv", "ogbn-products"):
         dataset = load_ogb_dataset(data_dir, dataname)
-    elif dataname == 'pokec':
+    elif dataname == "pokec":
         dataset = load_pokec_mat(data_dir)
+    elif dataname == "reddit":
+        dataset = load_reddit_dataset(data_dir, dataname)
     else:
-        raise ValueError('Invalid dataname')
+        raise ValueError("Invalid dataname")
+    return dataset
+
+
+def load_reddit_dataset(data_dir, nclass=5):
+    filename = "reddit"
+    dataset = NCDataset(filename)
+    reddit_dataset = Reddit(root=f"{data_dir}/reddit")
+    reddit_dataset = reddit_dataset[0]
+    dataset.graph["edge_index"] = torch.as_tensor(reddit_dataset.edge_index)
+    dataset.graph["node_feat"] = torch.as_tensor(reddit_dataset.x)
+    dataset.graph["num_nodes"] = reddit_dataset.x.shape[0]
+    dataset.label = reddit_dataset.y
+    dataset.load_fixed_splits = {
+        "train": reddit_dataset.train_mask,
+        "valid": reddit_dataset.val_mask,
+        "test": reddit_dataset.test_mask,
+    }
     return dataset
 
 
 def load_wikics_dataset(data_dir):
-    wikics_dataset = WikiCS(root=f'{data_dir}/wikics/')
+    wikics_dataset = WikiCS(root=f"{data_dir}/wikics/")
     data = wikics_dataset[0]
 
     edge_index = data.edge_index
@@ -93,17 +125,20 @@ def load_wikics_dataset(data_dir):
     label = data.y
     num_nodes = data.num_nodes
 
-    dataset = NCDataset('wikics')
-    dataset.graph = {'edge_index': edge_index,
-                     'node_feat': node_feat,
-                     'edge_feat': None,
-                     'num_nodes': num_nodes}
+    dataset = NCDataset("wikics")
+    dataset.graph = {
+        "edge_index": edge_index,
+        "node_feat": node_feat,
+        "edge_feat": None,
+        "num_nodes": num_nodes,
+    }
     dataset.label = label
 
     return dataset
 
+
 def load_hetero_dataset(data_dir, name):
-    #transform = T.NormalizeFeatures()
+    # transform = T.NormalizeFeatures()
     torch_dataset = HeterophilousGraphDataset(name=name.capitalize(), root=data_dir)
     data = torch_dataset[0]
 
@@ -115,16 +150,18 @@ def load_hetero_dataset(data_dir, name):
     dataset = NCDataset(name)
 
     ## dataset splits are implemented in data_utils.py
-    '''
+    """
     dataset.train_idx = torch.where(data.train_mask[:,0])[0]
     dataset.valid_idx = torch.where(data.val_mask[:,0])[0]
     dataset.test_idx = torch.where(data.test_mask[:,0])[0]
-    '''
+    """
 
-    dataset.graph = {'edge_index': edge_index,
-                     'node_feat': node_feat,
-                     'edge_feat': None,
-                     'num_nodes': num_nodes}
+    dataset.graph = {
+        "edge_index": edge_index,
+        "node_feat": node_feat,
+        "edge_feat": None,
+        "num_nodes": num_nodes,
+    }
     dataset.label = label
 
     return dataset
@@ -132,12 +169,14 @@ def load_hetero_dataset(data_dir, name):
 
 def load_amazon_dataset(data_dir, name):
     transform = T.NormalizeFeatures()
-    if name == 'amazon-photo':
-        torch_dataset = Amazon(root=f'{data_dir}Amazon',
-                                 name='Photo', transform=transform)
-    elif name == 'amazon-computer':
-        torch_dataset = Amazon(root=f'{data_dir}Amazon',
-                                 name='Computers', transform=transform)
+    if name == "amazon-photo":
+        torch_dataset = Amazon(
+            root=f"{data_dir}Amazon", name="Photo", transform=transform
+        )
+    elif name == "amazon-computer":
+        torch_dataset = Amazon(
+            root=f"{data_dir}Amazon", name="Computers", transform=transform
+        )
     data = torch_dataset[0]
 
     edge_index = data.edge_index
@@ -147,22 +186,27 @@ def load_amazon_dataset(data_dir, name):
 
     dataset = NCDataset(name)
 
-    dataset.graph = {'edge_index': edge_index,
-                     'node_feat': node_feat,
-                     'edge_feat': None,
-                     'num_nodes': num_nodes}
+    dataset.graph = {
+        "edge_index": edge_index,
+        "node_feat": node_feat,
+        "edge_feat": None,
+        "num_nodes": num_nodes,
+    }
     dataset.label = label
 
     return dataset
+
 
 def load_coauthor_dataset(data_dir, name):
     transform = T.NormalizeFeatures()
-    if name == 'coauthor-cs':
-        torch_dataset = Coauthor(root=f'{data_dir}Coauthor',
-                                 name='CS', transform=transform)
-    elif name == 'coauthor-physics':
-        torch_dataset = Coauthor(root=f'{data_dir}Coauthor',
-                                 name='Physics', transform=transform)
+    if name == "coauthor-cs":
+        torch_dataset = Coauthor(
+            root=f"{data_dir}Coauthor", name="CS", transform=transform
+        )
+    elif name == "coauthor-physics":
+        torch_dataset = Coauthor(
+            root=f"{data_dir}Coauthor", name="Physics", transform=transform
+        )
     data = torch_dataset[0]
 
     edge_index = data.edge_index
@@ -172,51 +216,57 @@ def load_coauthor_dataset(data_dir, name):
 
     dataset = NCDataset(name)
 
-    dataset.graph = {'edge_index': edge_index,
-                     'node_feat': node_feat,
-                     'edge_feat': None,
-                     'num_nodes': num_nodes}
+    dataset.graph = {
+        "edge_index": edge_index,
+        "node_feat": node_feat,
+        "edge_feat": None,
+        "num_nodes": num_nodes,
+    }
     dataset.label = label
 
     return dataset
 
+
 def load_ogb_dataset(data_dir, name):
     dataset = NCDataset(name)
-    ogb_dataset = NodePropPredDataset(name=name, root=f'{data_dir}/ogb')
+    ogb_dataset = NodePropPredDataset(name=name, root=f"{data_dir}/ogb")
     dataset.graph = ogb_dataset.graph
-    dataset.graph['edge_index'] = torch.as_tensor(dataset.graph['edge_index'])
-    dataset.graph['node_feat'] = torch.as_tensor(dataset.graph['node_feat'])
+    dataset.graph["edge_index"] = torch.as_tensor(dataset.graph["edge_index"])
+    dataset.graph["node_feat"] = torch.as_tensor(dataset.graph["node_feat"])
 
     def ogb_idx_to_tensor():
         split_idx = ogb_dataset.get_idx_split()
-        tensor_split_idx = {key: torch.as_tensor(
-            split_idx[key]) for key in split_idx}
+        tensor_split_idx = {key: torch.as_tensor(split_idx[key]) for key in split_idx}
         return tensor_split_idx
+
     dataset.load_fixed_splits = ogb_idx_to_tensor  # ogb_dataset.get_idx_split
     dataset.label = torch.as_tensor(ogb_dataset.labels).reshape(-1, 1)
     return dataset
 
+
 def load_pokec_mat(data_dir):
-    """ requires pokec.mat """
-    if not path.exists(f'{data_dir}/pokec/pokec.mat'):
-        drive_id = '1575QYJwJlj7AWuOKMlwVmMz8FcslUncu'
+    """requires pokec.mat"""
+    if not path.exists(f"{data_dir}/pokec/pokec.mat"):
+        drive_id = "1575QYJwJlj7AWuOKMlwVmMz8FcslUncu"
         gdown.download(id=drive_id, output="data/pokec/")
-        #import sys; sys.exit()
-        #gdd.download_file_from_google_drive(
+        # import sys; sys.exit()
+        # gdd.download_file_from_google_drive(
         #    file_id= drive_id, \
         #    dest_path=f'{data_dir}/pokec/pokec.mat', showsize=True)
 
-    fulldata = scipy.io.loadmat(f'{data_dir}/pokec/pokec.mat')
+    fulldata = scipy.io.loadmat(f"{data_dir}/pokec/pokec.mat")
 
-    dataset = NCDataset('pokec')
-    edge_index = torch.tensor(fulldata['edge_index'], dtype=torch.long)
-    node_feat = torch.tensor(fulldata['node_feat']).float()
-    num_nodes = int(fulldata['num_nodes'])
-    dataset.graph = {'edge_index': edge_index,
-                     'edge_feat': None,
-                     'node_feat': node_feat,
-                     'num_nodes': num_nodes}
+    dataset = NCDataset("pokec")
+    edge_index = torch.tensor(fulldata["edge_index"], dtype=torch.long)
+    node_feat = torch.tensor(fulldata["node_feat"]).float()
+    num_nodes = int(fulldata["num_nodes"])
+    dataset.graph = {
+        "edge_index": edge_index,
+        "edge_feat": None,
+        "node_feat": node_feat,
+        "num_nodes": num_nodes,
+    }
 
-    label = fulldata['label'].flatten()
+    label = fulldata["label"].flatten()
     dataset.label = torch.tensor(label, dtype=torch.long)
     return dataset
